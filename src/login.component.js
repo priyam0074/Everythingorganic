ebf232600400435cc6e73653bc808e240a706ed9
priyam vashishtha <priyam.vashishtha2011@gmail.com>
2017-05-03 17:03:39 +0530

    First commit

--- src/login.component.js
+++ src/login.component.js
@@ -10,18 +10,42 @@ export default class LoginComponent extends React.Component {
 		
 		return (
 			<div className="headerMain">
-				<header className = "headerAlign">
-					<div className = "headerItems" >
-						<div>
-							<img src= "../image/organic.jpg" className="imageLogo" alt="everythingOrganic"/>
-							<div className="logoText text-left"> Everything 
-								<span><small>Organic</small></span>
-							</div>
+    <header className="headerAlign">
+        <div className="headerItems">
+            <div id="blockAlignment">
+                <img src="../image/organic.jpg" className="imageLogo" alt="everythingOrganic" />
+                <div className="logoText text-left"> Everything
+                    <span><small>Organic</small></span>
+                </div>
+            </div>
+            <div className="navAlignment">
+             <a href = "#" className="subItems"> fruits </a>
+                   
+            </div>
+            <div className="navAlignment">
+             <a href = "#" className="subItems"> vegetables </a>
+                   
+            </div>
+            <div className="navAlignment">
+             <a href = "#" className="subItems"> cosmetics </a>
+                   
+            </div>
+            <div className="searchBox">
+            <input type="text" placeholder="search"/>
+            <a className="searchIcon">
+            <span className="fa fa-search" aria-hidden="true">
+            </span></a>
+            </div>
 
-							</div>
-						</div>
-					
-					</header>
-				</div>)
+            <div className="loginIcon">
+             <span className="fa fa-user" aria-hidden="true"></span> 
+            </div>
+            <div className="chartIcon">
+            <span className="fa fa-cart-arrow-down" aria-hidden="true"></span>
+            </div>
+        </div>
+
+    </header>
+</div>)
 	}
 }
